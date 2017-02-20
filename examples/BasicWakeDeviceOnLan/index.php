89320f86e7aa8032deb05435abe77a27868e7e33
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-02-20 16:03:15 +0100

    example: Add basic example
    Add example of WOL to wake an unique device.

---
+++ examples/BasicWakeDeviceOnLan/index.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * \brief Example to wake a specific device on LAN
+ *
+ * \author Quentin Comte-Gaz <quentin@comte-gaz.com>
+ * \date 20 February 2017
+ * \license MIT License (contact me if too restrictive)
+ * \copyright Copyright (c) 2017 Quentin Comte-Gaz
+ * \version 1.0
+ */
+?>
+
+<?php
+
+require_once __DIR__.'/../utils/wakeOnLan.php';
+
+$macAddress = '01:02:03:04:05:06';
+$broadcastIp = '192.168.1.255';
+$port = 7;
+
+if (wakeOnLan($macAddress, $broadcastIp, $port))
+{
+  echo("'{$macAddress}' is waking up!");
+}
+else
+{
+  echo("Coud not contact '{$macAddress}' (with broadcast ip '{$broadcastIp}' and port '{$port}').");
+}
+
+?>
