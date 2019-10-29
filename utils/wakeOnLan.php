6cbbfdc33dbb06f587161d6979f5a08300c8aa41
Quentin Comte-Gaz <quentin@comte-gaz.com>
2019-10-29 17:41:36 +0100

    lib: Fix some code issue (found with codacy)

--- utils/wakeOnLan.php
+++ utils/wakeOnLan.php
@@ -15,29 +15,30 @@
 /*!
  * \brief wakeOnLan Wake up a device which is connected on LAN
  *
- * \param macAddressHexadecimal (str) Mac address on hexadecimal format
+ * \param macAddrHexa (str) Mac address on hexadecimal format
  * \param broadcastAddress (str) Broadcast address on hexadecimal format
  * \param port (int, default) Port used to contact the device
  * \param timeout (int, default) Time to wait response from the device
  *
  * \return (bool) Wake up request received by the device
  */
-function wakeOnLan($macAddressHexadecimal, $broadcastAddress, $port = 7, $timeout = 2)
+function wakeOnLan($macAddrHexa, $broadcastAddress, $port = 7, $timeout = 2)
 {
   $result = false;
 
   // Check if mac address is valid and get its binary version
-  $macAddressHexadecimal = str_replace(':', '', $macAddressHexadecimal);
-  if (ctype_xdigit($macAddressHexadecimal) && strlen($macAddressHexadecimal) == 12) {
-    $macAddressBinary = pack('H12', $macAddressHexadecimal);
+  $macAddrHexa = str_replace(':', '', $macAddrHexa);
+  if (ctype_xdigit($macAddrHexa) && strlen($macAddrHexa) == 12) {
+    $macAddressBinary = pack('H12', $macAddrHexa);
 
     // Create the magic packet to wake up the device on lan
     $magicPacket = str_repeat(chr(0xFF), 6).str_repeat($macAddressBinary, 16);
 
     // Send the packet if possible to connect with UDP protocol
-    if ($fp = fsockopen('udp://'.$broadcastAddress, $port, $errno, $errstr, $timeout))
+    $con = fsockopen('udp://'.$broadcastAddress, $port, $errno, $errstr, $timeout)
+    if ($con)
     {
-      if (fputs($fp, $magicPacket) >= strlen($magicPacket))
+      if (fputs($con, $magicPacket) >= strlen($magicPacket))
       {
         $result = true;
       }
@@ -45,7 +46,7 @@ function wakeOnLan($macAddressHexadecimal, $broadcastAddress, $port = 7, $timeou
       {
         error_log("Can't send UDP packet");
       }
-      fclose($fp);
+      fclose($con);
     }
     else
     {
@@ -54,7 +55,7 @@ function wakeOnLan($macAddressHexadecimal, $broadcastAddress, $port = 7, $timeou
   }
   else
   {
-    error_log("Mac address '{$macAddressHexadecimal}' invalid: Only 0-9 and A-F are allowed (max size of 12 hexadecimal elements)");
+    error_log("Mac address '{$macAddrHexa}' invalid: Only 0-9 and A-F are allowed (max size of 12 hexadecimal elements)");
   }
   
   return $result;
